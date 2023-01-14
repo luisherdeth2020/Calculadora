4b4721c092fd7788bdb7f61fd27687fb13994f11
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-14 15:59:33 +0100

    Finished calculator v1.5

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,26 @@
 // import { Teclas } from "./Teclas";
 
-export function Board({color,data}) {
-	let btnColor = '';
-	// console.log(tecla.color);
-	switch (color) {
-		case 'w':
-			btnColor = 'styles.btnwhite';
-			break;
-		default:
-			break;
-	}
+const Board = ({tecla}) => {
+	const {tecla} = [
+		{ color: 'btnGray', data: 'AC' },
+		{ color: 'btnGray', data: 'DEL' },
+		{ color: 'btnGray', data: '%' },
+		{ color: 'btnYellow', data: '/' },
+		{ color: 'btnBlack', data: 7 },
+		{ color: 'btnBlack', data: 8 },
+		{ color: 'btnBlack', data: 9 },
+		{ color: 'btnYellow', data: '*' },
+		{ color: 'btnBlack', data: 4 },
+		{ color: 'btnBlack', data: 5 },
+		{ color: 'btnBlack', data: 6 },
+		{ color: 'btnYellow', data: '-' },
+		{ color: 'btnBlack', data: 1 },
+		{ color: 'btnBlack', data: 2 },
+		{ color: 'btnBlack', data: 3 },
+		{ color: 'btnYellow', data: '+' },
+		{ color: 'btnBlack', data: 0 },
+		{ color: 'btnBlack', data: '.' },
+		{ color: 'btnYellow', data: '=' },
+	];
 	return <div></div>;
-}
+};
